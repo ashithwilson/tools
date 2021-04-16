3f627ecb2d0b5dd92f06977486f451dcffc0cd90
Ashith <ashith@cloudera.com>
2021-04-16 17:09:49 +0530

    Beautify code

--- vm/virtualbox-cli.sh
+++ vm/virtualbox-cli.sh
@@ -1,19 +1,22 @@
+#!/bin/bash
+
 vm_name="Ubuntu 20"
 
 main() {
-  if vm_running
-  then
+  if vm_running; then
     start_ssh
   else
     run_vm && start_ssh
   fi
 
   #On user exit,
-  shutdown_vm
+  shutdown_prompt
 }
 
-run_vm() {
-  VBoxManage startvm "$vm_name"
+vm_running() {
+  VBoxManage showvminfo "$vm_name" --details |
+    grep -q "running" &&
+    return 0
 }
 
 start_ssh() {
@@ -23,23 +26,19 @@ start_ssh() {
   ssh ashes@localhost -p2222
 }
 
-shutdown_vm() {
-  read -n1 -t 3 -p "Do you want to shutdown the VM? [y/n] " input
+run_vm() {
+  VBoxManage startvm "$vm_name"
+}
+
+shutdown_prompt() {
+  read -r -n1 -t 3 -p "Do you want to shutdown the VM? [y/n] " input
   echo ""
-  if [[ "$input" == "y" ]] || [[ "$input" == "Y" ]]
-  then
-    echo -ne "Shutting down VM... ";
+  if [[ "$input" == "y" ]] || [[ "$input" == "Y" ]]; then
+    echo -ne "Shutting down VM... "
     VBoxManage controlvm "$vm_name" savestate
   else
     echo "Not shuting down VM"
   fi
-}
-
-vm_running(){
-  vmstatus=$(VBoxManage showvminfo "$vm_name" --details | grep -c "running")
-  [[ "$vmstatus" == 0 ]] && return 1
-  [[ "$vmstatus" > 0 ]] && return 0
-
 }
 
 main "$@"
